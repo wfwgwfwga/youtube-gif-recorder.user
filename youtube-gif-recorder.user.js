194ba82b1355b61a9df16226a8a351aebaf426cc
wfwgwfwga <171452050+wfwgwfwga@users.noreply.github.com>
2026-08-31 05:22:21 +0900

    Update youtube-gif-recorder.user.js

--- youtube-gif-recorder.user.js
+++ youtube-gif-recorder.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YouTube 녹화 · 스크린샷 · 움짤 생성
 // @namespace    http://tampermonkey.net/
-// @version      1.0.7
+// @version      1.0.8
 // @description  유튜브 플레이어 컨트롤바에 녹화/스크린샷/움짤 버튼 추가. 단축키 커스터마이징 가능 (기본값: 녹화 F9, 스크린샷 F10, 움짤 F8). 움짤 자동 생성 옵션 지원.
 // @match        https://www.youtube.com/*
 // @grant        GM_openInTab
@@ -2806,7 +2806,7 @@ if (
                 GM_openInTab(
                     blobUrl,
                     {
-                        active: !autoGenerate,
+                        active: true,
                         insert: true,
                         setParent: true
                     }
